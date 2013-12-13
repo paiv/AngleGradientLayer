64d09e4b841392fefc7b2e8b72e508f4539c2004
Pavel Ivashkov <pavels.code@gmail.com>
2013-12-13 13:39:35 +0200

    ~ sample: updated layouts when on iOS 7 and when rotating

--- AngleGradientSample/AngleGradientSample/SampleController.m
+++ AngleGradientSample/AngleGradientSample/SampleController.m
@@ -52,21 +52,28 @@
 {
 	CGRect rect = [UIScreen mainScreen].applicationFrame;
 	UIScrollView *v = [[UIScrollView alloc] initWithFrame:rect];
+	v.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 	v.backgroundColor = [UIColor whiteColor];
 
 	UserControl1 *uc1 = [[UserControl1 alloc] initWithFrame:v.bounds];
+	uc1.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 	[v addSubview:uc1];
-	
+
+	UIViewAutoresizing flexmargins = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+
 	UserControl2 *uc2 = [[UserControl2 alloc] initWithFrame:CGRectMake(5, 5, 88, 88)];
+	uc2.autoresizingMask = flexmargins;
 	[v addSubview:uc2];
 	
 	UserControl3 *uc3 = [[UserControl3 alloc] initWithFrame:CGRectMake(155, 95, 66, 66)];
 	[v addSubview:uc3];
 	
 	UserControl4 *uc4 = [[UserControl4 alloc] initWithFrame:CGRectMake(190, 305, 120, 120)];
+	uc4.autoresizingMask = flexmargins;
 	[v addSubview:uc4];
 	
 	_uc5 = [[UserControl5 alloc] initWithFrame:CGRectMake(15, 335, 100, 100)];
+	_uc5.autoresizingMask = flexmargins;
 	[v addSubview:_uc5];
 	
 	UserControl6 *uc6 = [[UserControl6 alloc] initWithFrame:CGRectMake(-10, 225, 135, 135)];
