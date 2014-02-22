9f9abffb41885cde2e06a2e830674dbe8d380ba7
Caylan Larson <caylan@semireg.com>
2014-02-22 09:01:16 -0600

    Added UIView subclass

---
+++ AngleGradient/AngleGradientView.h
@@ -0,0 +1,13 @@
+//
+//  AngleGradientView.h
+//  AngleGradientSample
+//
+//  Created by Caylan Larson on 2/22/14.
+//  Copyright (c) 2014 Archer Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AngleGradientView : UIView
+
+@end
---
+++ AngleGradient/AngleGradientView.m
@@ -0,0 +1,13 @@
+//
+//  AngleGradientView.m
+//  AngleGradientSample
+//
+//  Created by Caylan Larson on 2/22/14.
+//  Copyright (c) 2014 Archer Software. All rights reserved.
+//
+
+#import "AngleGradientView.h"
+
+@implementation AngleGradientView
+
+@end
