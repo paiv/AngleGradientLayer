bd0c977c0dcf9c964516b32064640db4353bb4af
James Hartt <jameshartt@gmail.com>
2016-01-24 11:42:25 +0000

    Add framework with shared scheme for Carthage

---
+++ AngleGradient/AngleGradient/AngleGradient.h
@@ -0,0 +1,19 @@
+//
+//  AngleGradient.h
+//  AngleGradient
+//
+//  Created by James Hartt on 24/01/2016.
+//  Copyright © 2016 James Hartt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AngleGradient.
+FOUNDATION_EXPORT double AngleGradientVersionNumber;
+
+//! Project version string for AngleGradient.
+FOUNDATION_EXPORT const unsigned char AngleGradientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AngleGradient/PublicHeader.h>
+
+
