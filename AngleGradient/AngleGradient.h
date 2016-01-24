9e3b961b23d77e3de87810dad3057c7906acbb8b
James Hartt <jameshartt@gmail.com>
2016-01-24 12:11:18 +0000

    Make header public

--- AngleGradient/AngleGradient.h
+++ AngleGradient/AngleGradient.h
@@ -14,6 +14,5 @@ FOUNDATION_EXPORT double AngleGradientVersionNumber;
 //! Project version string for AngleGradient.
 FOUNDATION_EXPORT const unsigned char AngleGradientVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <AngleGradient/PublicHeader.h>
-
+#import <AngleGradient/AngleGradientLayer.h>
 
